348fdb9b0cf26b59081311bff7b5754279bf7008
Hamdaoui-mathieu <mathieu.hamdaoui@gmail.com>
2023-06-30 12:22:18 +0200

    M a J

--- src/Controller/PanierController.php
+++ src/Controller/PanierController.php
@@ -2,17 +2,63 @@
 
 namespace App\Controller;
 
+use App\Repository\PlatRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\RequestStack;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class PanierController extends AbstractController
 {
-    #[Route('/panier', name: 'app_panier')]
-    public function index(): Response
+
+    private $requestStack;
+    private $PlatRepository;
+
+    public function __construct(RequestStack $requestStack, PlatRepository $PlatRepository)
     {
-        return $this->render('accueil/panier.html.twig', [
-            'controller_name' => 'PanierController',
+        $this->requestStack = $requestStack;
+        $this->PlatRepository = $PlatRepository;
+    }
+
+
+    #[Route('/panier', name: 'app_panier')]
+
+    public function panier(): Response
+    {
+
+        $session = $this->requestStack->getSession();
+        $panier = $session->get('panier', []);
+        $paniertotal = [];
+
+        foreach ($panier as $id => $quantite) {
+            $paniertotal[] = [
+                'plat' => $this->PlatRepository->find($id),
+                'quantite' => $quantite
+            ];
+        }
+        $total = 0;
+        foreach($paniertotal as $item) {
+            $totalItem = $item['plat']->getPrix() * $item['quantite'];
+            $total += $totalItem;
+        }
+        return $this->render('panier/panier.html.twig', [
+            'items' => $paniertotal,
+            'total' => $total
         ]);
+    }
+    #[Route('/ajout_panier/{id}', name: 'app_ajout_panier')]
+    public function addItems(int$id)
+    {
+        $session = $this->requestStack->getSession();
+        $panier = $session->get('panier', []);
+        if(!empty($panier[$id])){
+            $panier[$id]++;
+        }else{
+            $panier[$id] = 1;
+        
+        }
+        $session->set('panier', $panier);
+
+        return $this->redirectToRoute( 'app_panier');
     }
 }
